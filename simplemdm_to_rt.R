65c9598b5b3146e0f36937a2166a7f955e7a29e9
John Yocum <jtyocum@uw.edu>
2025-03-28 11:48:31 -0700

    initial commit

---
+++ simplemdm_to_rt.R
@@ -0,0 +1,16 @@
+library(here)
+library(tidyverse)
+
+simplemdm <- read_csv(here("data/input/simplemdm_export.csv"))
+
+simplemdm_cleaned <- simplemdm %>%
+  filter(grepl('DEOHS', `Device group`)) %>%
+  select(`SimpleMDM Name`,`Model`,`Product type`,`Serial number`,`assigned_user`) %>%
+  rename(name = `SimpleMDM Name`, model = `Model`, type = `Product type`, serial = `Serial number`, user = `assigned_user`) %>%
+  mutate(name = str_remove(name, '.clients.uw.edu')) %>%
+  mutate(type = str_replace(type, 'MacBook.*', 'Laptop')) %>%
+  mutate(type = str_replace(type, 'iMac.*', 'Desktop / Workstation')) %>%
+  mutate(type = str_replace(type, 'Mac mini.*', 'Desktop / Workstation')) %>%
+  mutate(make = 'Apple')
+
+write_csv(simplemdm_cleaned, here("data/output/simplemdm_cleaned.csv"))
