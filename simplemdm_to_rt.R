c01763ec4612b1662f46fbe77699e65aa0a1aa99
John Yocum <jtyocum@uw.edu>
2025-09-08 10:13:43 -0700

    adjust group names

--- simplemdm_to_rt.R
+++ simplemdm_to_rt.R
@@ -4,7 +4,7 @@ library(tidyverse)
 simplemdm <- read_csv(here("data/input/simplemdm_export.csv"))
 
 simplemdm_cleaned <- simplemdm %>%
-  filter(`Group: DEOHS Standard Installation` == TRUE | `Group: DEOHS Standard No Filevault` == TRUE | `Group: EPI Standard Deployment` == TRUE) %>%
+  filter(`Group: DEOHS Standard Software and Profiles` == TRUE | `Group: DEOHS Standard No Filevault` == TRUE | `Group: Epi Standard Software and Profiles` == TRUE) %>%
   select(`SimpleMDM Name`,`Model`,`Product type`,`Serial number`,`assigned_user`) %>%
   rename(name = `SimpleMDM Name`, model = `Model`, type = `Product type`, serial = `Serial number`, user = `assigned_user`) %>%
   mutate(name = str_remove(name, '.clients.uw.edu')) %>%
